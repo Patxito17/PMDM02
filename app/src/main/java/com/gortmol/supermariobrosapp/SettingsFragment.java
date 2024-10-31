d6ceb70e3ce268838cd9c23d60cf64ce2404e255
Patxito17 <estampador.avellana_0n@icloud.com>
2024-10-31 08:01:41 +0100

    Menu about

--- app/src/main/java/com/gortmol/supermariobrosapp/SettingsFragment.java
+++ app/src/main/java/com/gortmol/supermariobrosapp/SettingsFragment.java
@@ -10,6 +10,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        setPreferencesFromResource(R.xml.fragment_settings, rootKey);
 
     }
 }
